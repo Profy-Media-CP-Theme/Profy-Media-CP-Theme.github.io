0697f5770168ece7c0b71efbbf316209aab3899d
ptrvx <petar.vukovic@hotmail.com>
2021-10-15 22:52:00 +0200

    working on mob sidenav

---
+++ mob-header/mob-header.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mobHeader").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mobHeader").style.width = "0";
+  }
+  
