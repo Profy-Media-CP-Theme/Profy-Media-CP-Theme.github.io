cc8baf4c861c5839609c396ba74fbcc505032caf
petar <petar.vukovic@hotmail.com>
2021-11-22 14:07:14 +0100

    24 desktop

--- 8/script.js
+++ 8/script.js
@@ -1,18 +1,20 @@
 jQuery(document).ready(function ($) {
 
-
-    var triggerBtnList = [].slice.call(document.querySelectorAll("#v-pills-tab button"));
-    var triggerImgList = [].slice.call(document.querySelectorAll("#v-pills-img div"));
-    triggerBtnList.forEach((triggerEl, index) => {
-        var tabTrigger = new bootstrap.Tab(triggerEl);
-
-        triggerImgList[index].addEventListener('click', function (event) {
-            event.preventDefault();
-            tabTrigger.show();
+    var elements8 = [].slice.call(document.querySelectorAll('.element-8'));
+    
+    elements8.forEach(element => {
+        var triggerBtnList = [].slice.call(document.querySelectorAll("#v-pills-tab-" + element.id + " button"));
+        var triggerImgList = [].slice.call(document.querySelectorAll("#v-pills-img-" + element.id + " div"));
+        
+        triggerBtnList.forEach((triggerEl, index) => {
+            var tabTrigger = new bootstrap.Tab(triggerEl);
+    
+            triggerImgList[index].addEventListener('click', function (event) {
+                event.preventDefault();
+                tabTrigger.show();
+            });
+    
         });
-
     });
-
-    // bootstrap.Tab.getInstance(triggerTabList[0]).show();
 
 });
