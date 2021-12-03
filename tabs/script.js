a87cce246fb567f4e94b861aaeaa7420a24d4c46
petar <petar.vukovic@hotmail.com>
2021-12-03 23:38:01 +0100

    observer tabs

---
+++ tabs/script.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function ($) {
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                // Add the animation class
+                entry.target.classList.remove('start-transition');
+            }
+        });
+    });
+
+    const startAnimationElements = document.querySelectorAll('.progress-25 > .start-transition');
+    startAnimationElements.forEach((el) => {
+      observer.observe(el);
+    })
+});
