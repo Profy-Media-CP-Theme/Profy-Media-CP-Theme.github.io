880337a43ac4e9d48ac10b24d78020f114920709
petar <petar.vukovic@hotmail.com>
2021-12-05 03:47:07 +0100

    26 all

--- 26/script.js
+++ 26/script.js
@@ -5,6 +5,7 @@ jQuery(document).ready(function ($) {
     elements26.forEach(element => {
         var triggerBtnList = [].slice.call(document.querySelectorAll("#" + element.id + "-dots-26 > div.dot-26"));
         var triggerImgList = [].slice.call(document.querySelectorAll("#" + element.id + "-imgs-26 > img.img-26"));
+        var staticImgList = [].slice.call(document.querySelectorAll("#" + element.id + "-static-26 > img.still-img-26"));
         var slider26 = document.querySelector("#" + element.id + "-imgs-26");
         var selected = 0;
         var maxItems = triggerBtnList.length;
@@ -12,19 +13,29 @@ jQuery(document).ready(function ($) {
         triggerBtnList.forEach((triggerEl, index) => {
             triggerEl.addEventListener('click', function(event) {
                 event.preventDefault();
-
-                slider26.scrollTo({behavior: 'smooth', left: triggerImgList[index].offsetLeft - triggerImgList[0].offsetLeft});
                 selected = index;
+                scrollImg26();
             });
 
         });
 
-        
+        function scrollImg26() {
+            slider26.scrollTo({behavior: 'smooth', left: triggerImgList[selected].offsetLeft - triggerImgList[0].offsetLeft});
+            
+            staticImgList.forEach((img, index) => {
+                if(index == selected) {
+                    img.classList.add("active-26");
+                } else {
+                    img.classList.remove("active-26");
+                }
+            });
+        }
 
         setInterval(function() {
             selected = (selected + 1) % maxItems;
-            slider26.scrollTo({behavior: 'smooth', left: triggerImgList[selected].offsetLeft - triggerImgList[0].offsetLeft});
+            scrollImg26(selected);
         }, 3000);
+
 
     });
 
