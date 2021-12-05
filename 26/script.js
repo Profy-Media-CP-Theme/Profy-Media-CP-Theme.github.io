0bcee962b74400988983d62795c59ccf4283e9d4
petar <petar.vukovic@hotmail.com>
2021-12-05 03:24:34 +0100

    timer 26

--- 26/script.js
+++ 26/script.js
@@ -6,15 +6,27 @@ jQuery(document).ready(function ($) {
         var triggerBtnList = [].slice.call(document.querySelectorAll("#" + element.id + "-dots-26 > div.dot-26"));
         var triggerImgList = [].slice.call(document.querySelectorAll("#" + element.id + "-imgs-26 > img.img-26"));
         var slider26 = document.querySelector("#" + element.id + "-imgs-26");
-        
+        var selected = 0;
+        var maxItems = triggerBtnList.length;
+
         triggerBtnList.forEach((triggerEl, index) => {
             triggerEl.addEventListener('click', function(event) {
                 event.preventDefault();
 
                 slider26.scrollTo({behavior: 'smooth', left: triggerImgList[index].offsetLeft - triggerImgList[0].offsetLeft});
+                selected = index;
             });
-    
+
         });
+
+        
+
+        setInterval(function() {
+            selected = (selected + 1) % maxItems;
+            slider26.scrollTo({behavior: 'smooth', left: triggerImgList[selected].offsetLeft - triggerImgList[0].offsetLeft});
+        }, 3000);
+
     });
+
 
 });
