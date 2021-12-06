c127a23413618f546b6c161c42758716526b57f1
petar <petar.vukovic@hotmail.com>
2021-12-06 04:09:46 +0100

    23

--- 23/script.js
+++ 23/script.js
@@ -1,14 +1,18 @@
 jQuery(document).ready(function ($) {
 
-    var elements23 = [].slice.call($(".element-23"));
+    var elements23 = [].slice.call($(".wrapper-23"));
 
     elements23.forEach(element => {
 
         trig23 = true;
+        grid23 = $("#"+element.id);
+        offsetHeight = grid23.offset().top + 0.25*grid23.children().first().outerHeight();
+        offsetEnd = grid23.offset().top + 0.8*grid23.children().first().outerHeight();
 
         $(window).scroll(function () {
             if (trig23) {
-                if ($(window).scrollTop() > $('#'+element.id).offset().top) {
+                scrollTopVal = $(window).scrollTop();
+                if (scrollTopVal > offsetHeight && scrollTopVal < offsetEnd) {
                     trig23 = false;
                     doit(element.id);
                 }
@@ -20,23 +24,23 @@ jQuery(document).ready(function ($) {
 
     function doit(id) {
 
-        var x23 = $('#'+id).addClass("start-23");
+        var x23 = $('#'+id + ' > .element-23').addClass("start-23");
 
-        $('#' + id + '.element-23 img').css({
+        $('#' + id + ' > .element-23 img').css({
             "transition": "1s",
         });
     
         setTimeout(function () {
     
-            jQuery('#' + id + ' .extratext-23').css("display", "none");
-            jQuery('#' + id + '.element-23 .right-23 img').css("display", 'initial');
-            jQuery('#' + id + ' .text-23').css("transition", "1s");
+            $('#' + id + ' .extratext-23').css("display", "none");
+            $('#' + id + ' .right-23 img').css("display", 'initial');
+            $('#' + id + ' .text-23').css("transition", "1s");
     
             setTimeout(function () {
                 x23.addClass("transform-23");
             }, 100);
     
-        }, 700);
+        }, 900);
     }
 
 });
