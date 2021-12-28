06a1b6bfcf19c719230ce71b099ce04aded8a0ca
petar <petar.vukovic@hotmail.com>
2021-12-28 15:05:21 +0100

    coarousel d

--- carousel/script.js
+++ carousel/script.js
@@ -1,27 +1,59 @@
 jQuery(document).ready(function ($) {
 
-    $(".owl-carousel").owlCarousel({
+    var owl = $(".owl-carousel");
 
-        loop:true,
+    owl.owlCarousel({
+
+        loop: true,
         items: 1,
         center: true,
-        video: true,
-        animateOut: 'fadeOut',
-
-        // autoplay: true,
-
+        autoplayTimeout: 5000,
+        autoplay: false,
         nav: true,
+        lazyLoad: true,
+        lazyLoadEager: 1,
+        navText: ["<i class='bi bi-chevron-left'></i>","<i class='bi bi-chevron-right'></i>"],
         
-
-
+        onTranslate: translateCb,
+        onInitialized: startFirst,
         
-
-
-
-
 
     });
 
+    var contents;
+    var currentSlide = -1;
 
+    function translateCb(event) {
+        index = event.page.index;
+        translationHandler(index);
+    }
 
+    function translationHandler(index) {
+        currentSlide = index;
+        console.log('handling: ' + index);
+        current = contents[index];
+        if (current.tagName == "VIDEO") {
+            owl.trigger("stop.owl.autoplay");
+            current.pause();
+            current.currentTime = '0';
+            current.play();
+            current.addEventListener('ended', (event) => {
+                if (currentSlide === index) {
+                    nextSlide();
+                }
+            }, {"once": true,});
+        } else {
+            owl.trigger("play.owl.autoplay");
+        }
+    }
+
+    function nextSlide() {
+        owl.trigger("next.owl.carousel");
+    }
+
+    function startFirst(event) {
+        contents = jQuery(".owl-carousel .owl-item:not(.cloned) .carousel-content");
+        translationHandler(0);
+    }
+    
 });
